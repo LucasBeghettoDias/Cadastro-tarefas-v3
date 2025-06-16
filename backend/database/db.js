e587aca11072b1113103afa4157c066fef683b7b
LucasBeghettoDias <lucasbeghettodias@gmail.com>
2025-06-16 13:14:41 -0300

    Adicionando arquivos que faltavam

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,5 +1,4 @@
 // const { Sequelize } = require("sequelize");
-<<<<<<< HEAD
 
 // const sequelize = new Sequelize({
 //   dialect: "sqlite",
@@ -8,15 +7,15 @@
 
 // module.exports = sequelize;
 
-const { Sequelize } = require("sequelize");
+// const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('cadastro_tarefas', 'tarefas_user', '123456', {
-  host: "localhost",
-  dialect: "mysql"
-});
+// const sequelize = new Sequelize('cadastro_tarefas', 'tarefas_user', '123456', {
+//   host: "localhost",
+//   dialect: "mysql"
+// });
 
-module.exports = sequelize;
-=======
+// module.exports = sequelize;
+
 
 // const sequelize = new Sequelize({
 //   dialect: "sqlite",
@@ -33,4 +32,4 @@ const sequelize = new Sequelize ("cadastro_tarefas", "aluno", "senha123", {
 })
 
 module.exports = sequelize 
->>>>>>> 76ef1bae4a57e154af0cd5b14f116d7959941688
+
